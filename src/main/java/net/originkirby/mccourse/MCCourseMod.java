67ea18dc744628c6a2041ed550d2c899a5274d14
OriginZer0 <zero0rigin127@gmail.com>
2026-04-09 15:10:36 -0400

    fixed misspelling

--- src/main/java/net/originkirby/mccourse/MCCourseMod.java
+++ src/main/java/net/originkirby/mccourse/MCCourseMod.java
@@ -1,6 +1,7 @@
 package net.originkirby.mccourse;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,6 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.originkirby.mccourse.item.ModItems;
 import org.slf4j.Logger;
 
 @Mod(MCCourseMod.MOD_ID)
@@ -22,6 +24,9 @@ public class MCCourseMod {
 
     public MCCourseMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
+
+        ModItems.register(modEventBus);
+
 
         modEventBus.addListener(this::commonSetup);
 
@@ -36,7 +41,10 @@ public class MCCourseMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
