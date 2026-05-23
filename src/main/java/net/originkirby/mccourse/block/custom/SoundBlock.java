d18466363a3756c24796ab3e15d787f5ae50bf0a
OriginZer0 <zero0rigin127@gmail.com>
2026-05-23 12:09:09 -0400

    Tooltips

--- src/main/java/net/originkirby/mccourse/block/custom/SoundBlock.java
+++ src/main/java/net/originkirby/mccourse/block/custom/SoundBlock.java
@@ -1,16 +1,23 @@
 package net.originkirby.mccourse.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties pProperties) {
@@ -41,5 +48,12 @@ public class SoundBlock extends Block {
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
         pLevel.playSound(null, pPos, SoundEvents.NOTE_BLOCK_BIT.get(), SoundSource.BLOCKS);
         super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.translatable("tooltip.mccourse.sound_block"));
+
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 }
