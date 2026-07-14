1f6d6aadd14832330702f107fef94187d249a9a5
OriginZer0 <zero0rigin127@gmail.com>
2026-07-14 10:05:53 -0400

    Tools (Sword, Pickaxe, Axe, Shovel, and Hoe)

--- src/main/java/net/originkirby/mccourse/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/originkirby/mccourse/datagen/ModBlockTagGenerator.java
@@ -63,8 +63,10 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
 
         this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
                 .add(ModBlocks.NETHER_ALEXANDRITE_ORE.get(),
-                        ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get(),
-                        ModBlocks.END_ALEXANDRITE_ORE.get());
+                        ModBlocks.DEEPSLATE_ALEXANDRITE_ORE.get());
+
+        this.tag(ModTags.Blocks.NEEDS_ALEXANDRITE_TOOL)
+                .add(ModBlocks.END_ALEXANDRITE_ORE.get());
 
         this.tag(BlockTags.FENCES)
                 .add(ModBlocks.ALEXANDRITE_FENCE.get());
--- src/main/java/net/originkirby/mccourse/item/ModItems.java
+++ src/main/java/net/originkirby/mccourse/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.originkirby.mccourse.item;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -27,6 +27,17 @@ public class ModItems {
 
     public static final RegistryObject<Item> PEAT_BRICK = ITEMS.register("peat_brick",
             () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> ALEXANDRITE_SWORD = ITEMS.register("alexandrite_sword",
+            () -> new SwordItem(ModToolTiers.ALEXANDRITE, 2, 3, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> ALEXANDRITE_PICKAXE = ITEMS.register("alexandrite_pickaxe",
+            () -> new PickaxeItem(ModToolTiers.ALEXANDRITE, 1, 2, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> ALEXANDRITE_AXE = ITEMS.register("alexandrite_axe",
+            () -> new AxeItem(ModToolTiers.ALEXANDRITE, 3, 2, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> ALEXANDRITE_SHOVEL = ITEMS.register("alexandrite_shovel",
+            () -> new ShovelItem(ModToolTiers.ALEXANDRITE, 1, 3, new Item.Properties().durability(256)));
+    public static final RegistryObject<Item> ALEXANDRITE_HOE = ITEMS.register("alexandrite_hoe",
+            () -> new HoeItem(ModToolTiers.ALEXANDRITE, 1, 4, new Item.Properties().durability(256)));
 
 
 
